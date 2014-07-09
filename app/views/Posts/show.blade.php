a8e3b6262e322af71c811234545e9bbdefd1bd34
abeller3 <beller.amanda@yahoo.com>
2014-07-09 14:26:11 -0500

    adjusted what can be seen in this view

--- app/views/Posts/show.blade.php
+++ app/views/Posts/show.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>{{{ $post->title }}}</h1>
-<h1>{{{ $post->created_at }}}</h1>
+	<h1>{{{ $post->title }}}</h1>
+	<h5>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}<br>
+	<h3>Updated At: </h3>
+	<p>{{ link_to_action('PostsController@show', $post->updated_at->format('l, F jS Y @ h:i:s A'), array($post->id)) }}</p>
+	<h3>Post Content</h3>
+
+	@if($post->img_path)
+	<img src="{{{ $post->img_path }}}" class="img-responsive">
+	@endif
+
+	{{ $post->renderBody() }}<br>
+
+	@if (Auth::check())
+	<a href="{{action('PostsController@edit', $post->id)}}" class="btn btn-info btn-sm" role="button">Edit</a>
+	{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE')) }}
+	<button type='submit' class='btn btn-info'>Delete</button>
+	{{ Form::close() }}
+	@endif
+
 @stop
 
 
