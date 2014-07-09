4e64e9ecc3138a3b729edb32c05642938ba76aaa
abeller3 <beller.amanda@yahoo.com>
2014-07-09 14:25:38 -0500

    added many more features

--- app/views/Posts/index.blade.php
+++ app/views/Posts/index.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.master')
 @section('content')
 
-<h1>All the Posts</h1>
+<h1>Personal Blog Posts</h1>
+
+<div class="table-responsive">
+  <table class="table">
+	<tr>
+		<th>Title</th>
+		<th>Creation Date</th>
+		<th>Actions</th>
+	</tr>
+
 @foreach ($posts as $post)
-	{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}
-	<br>
-	@endforeach
-	<hr>
-	{{link_to_action('PostsController@create', 'New Post') }}
+<tr>
+	<td class='posts'>{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}</td>
+	<td class='posts'>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</td>
+	<td class='posts'>{{ link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' => 'btn btn-default btn-sm')) }}</td>
+</tr>
+@endforeach
+</table>
+	{{ Form::open(array('action' => 'PostsController@index', 'class' => 'form-inline', 'method' => 'GET')) }}
+    {{ Form::text('search', null, array('placeholder' => 'Search Query', 'class'=> 'form-control col-lq-4')) }}
+    {{ Form::submit() }}
+   	{{ Form::close() }}
+  </table>
+</div>
 @stop
