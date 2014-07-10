129d66ef8c0ba1a9261930d03b6cdd60a5f0c967
abeller3 <beller.amanda@yahoo.com>
2014-07-10 15:52:08 -0500

    Modified to resolve branch conflict.

--- app/views/Posts/index.blade.php
+++ app/views/Posts/index.blade.php
@@ -2,7 +2,6 @@
 @section('content')
 
 <h1>Personal Blog Posts</h1>
-
 <hr>          <!-- blog entry -->
 @foreach ($posts as $post)
 	<div class="blog-post">
@@ -10,35 +9,17 @@
 	    <hr>
 	  <p><span class="glyphicon glyphicon-time"></span>Post on {{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</p>
 	  <hr>
-	   {{ $post->renderBody() }}
+	  @if($post->img_path)
+	<img src="{{{ $post->img_path }}}" class="img-responsive">
+	@endif
+	   <p>{{{ $post->body }}}<p>
 	  	@if (Auth::check())
 	  <a class="btn btn-primary" href="{{ action('PostsController@edit', array($post->id)) }}">Edit <span class="glyphicon glyphicon-chevron-right"></span></a>
+
 	  @endif
 	</div>
 @endforeach
 
+<?php echo $posts->links(); ?>
 
-
-
-<!-- <div class="table-responsive">
-  <table class="table">
-	<tr>
-		<th>Title</th>
-		<th>Creation Date</th>
-		@if (Auth::check())
-			<th>Actions</th>
-		@endif
-	</tr>
-
-@foreach ($posts as $post)
-<tr>
-	<td class='posts'>{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}</td>
-	<td class='posts'>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</td>
-	@if (Auth::check())
-		<td class='posts'>{{ link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' => 'btn btn-default btn-sm')) }}</td>
-	@endif
-</tr>
-@endforeach
-</table>
-</div> -->
 @stop
