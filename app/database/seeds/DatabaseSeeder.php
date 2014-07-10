3c70c0c4b80b56ee7814a1f82c43f11ea5a36b0b
abeller3 <beller.amanda@yahoo.com>
2014-07-10 11:41:19 -0500

    multiple migrations

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,47 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+	    $this->call('PostTableSeeder');
 	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        $user = new User();
+        $user->email = 'admin@codeup.com';
+        $user->password = Hash::make('adminPass123!');
+        $user->save();
+
+        $user = new User();
+        $user->email = 'amandabeller@codeup.com';
+        $user->password = Hash::make('amanda');
+        $user->save();
+    }
+
+}
+
+class PostTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('posts')->delete();
+
+        $user = User::first();
+
+        for($i = 1; $i <= 10; $i++)
+        {
+	        $post = new Post();
+	        $post->user_id = $user->id;
+	        $post->title = 'I need a hug!' . ($i * 10);
+	        $post->body = 'Hug me!!!';
+	        $post->save();
+	    }
+    }
 
 }
